70db867309357536047e91c00c5d001cec8ade14
Neal Patrick Gary <nealpgary@gmail.com>
2015-09-09 19:34:33 +0000

    Removed PDFKit. Added MongoClient MongoDB driver and connection for MongoDB database.

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
-// Require MongoDB Driver
-var MongoClient = require('mongodb');
+// Require wkhtmltopdf (Webkit HTML to PDF)
+var wkhtmltopdf = require('wkhtmltopdf');
+
+// Require MongoDB driver
+var mongoClient = require('mongodb').MongoClient
+  , assert = require('assert');
+  
+// Require PDFKit
+// var PDFdoc = require('pdfkit');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -11,53 +18,32 @@ router.get('/', function(req, res, next) {
 
 /* POST a new record */
 router.post('/records', function(req, res, next) {
-    /* create variables for database here */
-    console.log(req.body.clmtfirst);
-    console.log(req.body.clmtmiddle);
-    console.log(req.body.clmtlast);
-    console.log(req.body.clmtaddr);
-    console.log(req.body.clmtcity);
-    console.log(req.body.clmtstate);
-    console.log(req.body.clmtzip);
-    console.log(req.body.clmthomephn);
-    console.log(req.body.clmtbusphn);
-    console.log(req.body.clmtage);
-    console.log(req.body.clmtssn);
-    console.log(req.body.clmtmarstat);
-    console.log(req.body.clmtspouse);
-    console.log(req.body.incdate);
-    console.log(req.body.inctime);
-    console.log(req.body.incloc);
-    console.log(req.body.incdesc);
-    console.log(req.body.propdam);
-    console.log(req.body.hasins);
-    console.log(req.body.insclaimfiled);
-    console.log(req.body.insco);
-    console.log(req.body.inspolnum);
-    console.log(req.body.insstart);
-    console.log(req.body.insend);
-    console.log(req.body.estdam);
-    console.log(req.body.wit1name);
-    console.log(req.body.wit2name);
-    console.log(req.body.wit3name);
-    console.log(req.body.injname);
-    console.log(req.body.injaddr);
-    console.log(req.body.injdesc);
-    console.log(req.body.doctor);
-    console.log(req.body.hosp);
-    console.log(req.body.hospaddr);
-    console.log(req.body.occ);
-    console.log(req.body.employer);
-    console.log(req.body.timelost);
-    console.log(req.body.timeloststart);
-    console.log(req.body.timelostend);
-    console.log(req.body.salwk);
-    console.log(req.body.salhr);
-    console.log(req.body.increp);
-    console.log(req.body.increpto);
-    console.log(req.body.increpdate);
-    
-    res.redirect('/');
+  // Add a record to the database
+  // Connect to MongoDB Server
+  var dbUrl = 'mongodb://localhost:27017/claimintentdb'
+  mongoClient.connect(dbUrl, function(err, db){
+    assert.equal(null, err);
+    console.log("Connected correctly to server.");
+    db.close();
+  });
+
+  
+  /* Testing PDFKit functionality */
+//  var doc = new PDFdoc;
+//  var clmtfirst = req.body.clmtfirst;
+//  var stream = fs.createWriteStream('/generated/' + clmtfirst +'.pdf');
+//  doc.pipe(stream);
+//  doc.text(clmtfirst);
+//  res.set('Content-Type', "application/pdf");
+//  var buff = new Buffer();
+//  res.send(buff);
+//  doc.end();
+
+  /* Testing wkhtmltopdf functionality */
+  
+  
+//  console.log('File saved.');
+  res.redirect('/');
 });
 
 module.exports = router;
