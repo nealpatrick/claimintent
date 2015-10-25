34a5468000a05b51093d70247126094d9744ec2e
Neal Patrick Garith <nealpgary@gmail.com>
2015-10-25 15:55:55 -0400

    Begin adding AngularJS and Angular Material

--- app.js
+++ app.js
@@ -21,10 +21,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(require('node-sass-middleware')({
-  src: path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
-  indentedSyntax: true,
-  sourceMap: true
+   src: path.join(__dirname, 'public'),
+   dest: path.join(__dirname, 'public'),
+   indentedSyntax: true,
+   sourceMap: true
 }));
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -37,15 +37,15 @@ app.use('/', routes);
 /* Server information for Cloud9 IDE. */
 // Application runs at 'https://claimintent-nealpatrick.c9.io'
 var server = app.listen(process.env.PORT, function(){
-  var host = process.env.IP;
-  var port = process.env.PORT;
+   var host = process.env.IP;
+   var port = process.env.PORT;
 });
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+   var err = new Error('Not Found');
+   err.status = 404;
+   next(err);
 });
 
 // error handlers
@@ -53,23 +53,23 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
+   app.use(function(err, req, res, next) {
+      res.status(err.status || 500);
+      res.render('error', {
+         message: err.message,
+         error: err
+      });
+   });
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+   res.status(err.status || 500);
+   res.render('error', {
+      message: err.message,
+      error: {}
+   });
 });
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -4,31 +4,31 @@ var router = express.Router();
 // Require wkhtmltopdf (Webkit HTML to PDF)
 var wkhtmltopdf = require('wkhtmltopdf');
 
-// Require MongoDB driver
-var mongoClient = require('mongodb').MongoClient
-  , assert = require('assert');
-  
-// Require PDFKit
-// var PDFdoc = require('pdfkit');
-
-/* GET home page. */
+// Require MongoDB driver, assert
+var mongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
+   
+/* GET home page */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Claim Intent' });
+   res.render('index', { title: 'Claim Intent' });
 });
 
 /* POST a new record */
 router.post('/record', function(req, res, next) {
-  // Add a record to the database
-  // Connect to MongoDB Server
-  var dbUrl = 'mongodb://localhost:27017/claimintentdb'
-  mongoClient.connect(dbUrl, function(err, db){
-    assert.equal(null, err);
-    console.log("Connected correctly to server.");
-    db.close();
-  });
+   // Add a record to the database
+   // Connect to MongoDB Server
+   var dbUrl = 'mongodb://localhost:27017/claimintentdb'
+   mongoClient.connect(dbUrl, function(err, db){
+      assert.equal(null, err);
+      console.log("Connected correctly to server.");
+      db.close();
+   });
 
-  
-  /* Testing PDFKit functionality */
+//  Parse data from body to 
+   
+/*  Testing PDFKit functionality  */
+//  Require PDFKit
+//  var PDFdoc = require('pdfkit');
 //  var doc = new PDFdoc;
 //  var clmtfirst = req.body.clmtfirst;
 //  var stream = fs.createWriteStream('/generated/' + clmtfirst +'.pdf');
@@ -39,11 +39,11 @@ router.post('/record', function(req, res, next) {
 //  res.send(buff);
 //  doc.end();
 
-  /* Testing wkhtmltopdf functionality */
-  
-  
+/* Testing wkhtmltopdf functionality */
+   
+   
 //  console.log('File saved.');
-  res.redirect('/');
+   res.redirect('/');
 });
 
 module.exports = router;
